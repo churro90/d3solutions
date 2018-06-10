e1e1280155e5c9de37f0799c98ab1d5a04a96e25
Martin Carrasco <martin.carrascof@gmail.com>
2018-06-10 01:24:48 -0400

    Cambio de puerot

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express'),
       path    = require('path'),
       app = express();
 
+      const PORT = process.env.PORT || 5000;    
       app.engine('html', require('ejs').renderFile);
       app.set('view engine', 'ejs');
       app.use(express.static(path.join(__dirname, 'public')));
@@ -11,5 +12,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port' + PORT);
 });
